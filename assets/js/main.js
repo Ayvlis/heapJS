b04a6b64444782daeedd615a1ff2d00d5f944af3
Silvia Gioia Florio <floriosg@gmail.com>
2016-10-23 16:54:24 +0200

    little js and css improvements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,28 @@
 $(document).ready(function() {
 
-	$(".page1").addClass('visible-block');
-
-	$(".hint").fadeOut('fast');
-
 	$("#add").focus();
+ 
+	var counter = 0; // number of elements in the array
 
-	var counter = 0;
 
 	$("#add").keypress(function (e) {
 		$('.hint').fadeOut();
-		var key = e.which;
+		var key = e.which; //save in key the key code of the key pressed
  		if(key == 13)  { // the enter key code
  			
- 			var number = parseInt($("#add").val());
+ 			var number = parseInt($("#add").val()); 
  			$("#add").val("");	
  			
- 			if(number>99 || isNaN(number)) {
+ 			if(number>99 || isNaN(number)) { //if the input is wrong
 
  				$('.hint').fadeIn("slow");
- 				$('.hint').css("display", "inline");
 
  			} else {
  				counter++;
- 				var log = Math.floor(Math.log(counter)/Math.LN2);
- 				var row = ".row-"+log;
  				$(".array").append('<p class="element">'+number+'</p>');
 
 
- 				if(counter==1) {
+ 				if(counter==1) { //if there is at least one element in the array show the buttons
  					$(".make-tree").removeClass('invisible');
  					$(".build-heap").removeClass('invisible');
  				}
@@ -37,72 +31,64 @@ $(document).ready(function() {
  		}
  	}); 
 
+
 	$(".make-tree").click(function() {
-		var nodes = [];
-		for (var i = counter-1; i >= 0; i--) {
-			var node = ".array p:nth-child("+(i+1)+")";
-			nodes[i]=$(node).text();
-			$(node).remove();
-		}
 
-		for (var i = 1; i<=counter ; i++) {
-			var log = Math.floor(Math.log(i)/Math.LN2);
-			var row = ".row-"+log;
-			$(row).append('<div class="spot"><p class="element" id="node-'+i+'"">'+nodes[i-1]+'</p></div>');
-			if(counter>1) {
+		$(".page1").addClass('invisible');
 
+		var nodes = arrayFrom(".array", "p"); //all the elements inserted by the user goes here
 
-				var parent = "#node-"+Math.floor(i/2);
-				var me = "#node-"+i;
-
-				$(me).connections({to: parent, within:".rows"});
-
-			}
-
-		}
-
-
-		$(".make-tree").addClass('invisible');
-		$(".build-heap").addClass('invisible');
-		$("#add").addClass('invisible');
-		$(".hint").remove();
-		$('.element').connections('update');
-
-	});
-
+		buildTree(nodes); //visualizing the tree
+    
+  });
 
 
 	$(".build-heap").click(function() {
-		var nodes = [];
+		
+    $(".page1").addClass('invisible');
+
+		var heaptree = new heap(arrayFrom(".array", "p")); //build a heap from the list of elements inserted by the user
+
+		buildTree(heaptree.heap); //visualize the heap
+    
+	});
+
+
+	function arrayFrom(parent, child) {
+		
+    var array = [];
 		for (var i = counter-1; i >= 0; i--) {
-			var node = ".array p:nth-child("+(i+1)+")";
-			nodes[i]=parseInt($(node).text());
-			$(node).remove();
+			var element = parent+" "+ child + ":nth-child("+(i+1)+")";
+			array[i]=$(element).text();
 		}
+    
+    $(parent).empty();
 
-		var heaptree = new heap(nodes);
-
-		for (var i = 1; i<=counter ; i++) {
+		return array;
+	
+  }
+  
+  
+  function buildTree(array) {
+   
+    for (var i = 1; i<=array.length; i++) { 
 			var log = Math.floor(Math.log(i)/Math.LN2);
 			var row = ".row-"+log;
-			$(row).append('<div class="spot"><p class="element" id="node-'+i+'"">'+heaptree.heap[i-1]+'</p></div>');
-			if(counter>1) {
-
-
+			$(row).append('<div class="spot"><p class="element" id="node-'+i+'"">'+array[i-1]+'</p></div>');
+			
+      if(array.length>1) {
 				var parent = "#node-"+Math.floor(i/2);
 				var me = "#node-"+i;
 
 				$(me).connections({to: parent, within:".rows"});
 
 			}
+    }
+    
+    $('.element').connections('update');
+  
+  }
 
-		}
-
-		$(".build-heap").addClass('invisible');
-		$(".make-tree").addClass('invisible');
-		$("#add").addClass('invisible');
-		$(".hint").remove();
-		$('.element').connections('update');
 
 	function heap(array) {
 		this.heapsize = array.length;
@@ -110,10 +96,8 @@ $(document).ready(function() {
 
 		this.heap = array;
 		buildHeap(this);
-		
-
-		
 	}
+
 
 	function buildHeap(heap) {
 		for (var i=Math.floor(heap.heapsize/2); i>=1; i--) {
@@ -121,6 +105,7 @@ $(document).ready(function() {
 			heapify(heap, i-1);
 		}
 	}
+
 
 	function heapify(heap, i) {
 		var m;
@@ -137,24 +122,28 @@ $(document).ready(function() {
 
 		if(m!=i) {
 			console.log(heap.heap[m] + " " +  heap.heap[i] );
-			scambia(heap.heap, i, m);
+			swap(heap.heap, i, m);
 			heapify(heap, m);
 		}
 	}
 
-	function scambia(array, i, m) {
+
+	function swap(array, i, m) {
 		var temp = array[i];
 		array[i] = array[m];
 		array[m] = temp;
 	}
 
+
 	function left(i) {
 		return 2*i+1;
 	}
 
+
 	function right(i) {
 		return 2*i+2;
 	}
+
 
 	function parent(i) {
 		return i/2;
@@ -162,10 +151,6 @@ $(document).ready(function() {
 
 
 
-	});
-
-
 
 
 }); 
-
